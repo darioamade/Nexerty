bdc18211c80c9fb3505290d884a3df90e2f71715
darioamade <dario_isd@outlook.com>
2020-06-24 11:29:33 +0100

    Commit
    added new changes

--- js/app.js
+++ js/app.js
@@ -1,64 +1,56 @@
-
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
 
 //Buttons
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
 
 //counter
 
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 
 //Button listeners
 
-nextBtn.addEventListener('mouseover',() => {
-    if (counter >= carouselImages.length -1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
+nextBtn.addEventListener("mouseover", () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-prevBtn.addEventListener('mouseover',() => {
-    if(counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px)';
-
+prevBtn.addEventListener("mouseover", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-carouselSlide.addEventListener('transitionend',() => { 
-   if (carouselImages[counter].id ==='lastClone'){
-       carouselSlide.style.transition = "none";
-       counter = carouselImages.length -2 ;
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
 
-       carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px)'; 
-   } 
-   if (carouselImages[counter].id ==='firstClone'){
-       carouselSlide.style.transition = "none";
-       counter = carouselImages.length -counter ;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
 
-       carouselSlide.style.transform = 'translateX('+ (-size * counter) + 'px)';
-      
-   }   
-
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
 });
 
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const nav = document.querySelector(".nav");
 
-const menuOpen = document.querySelector('.menu-open');
-const menuClose = document.querySelector('.menu-close');
-const nav = document.querySelector('.nav');
+menuOpen.addEventListener("click", menuTog);
+menuClose.addEventListener("click", menuTog);
 
-menuOpen.addEventListener('mouseover', menuTog);
-menuClose.addEventListener('click', menuTog);
-
-function menuTog(e){
-  nav.classList.toggle('active')
-};
+function menuTog(e) {
+  nav.classList.toggle("active");
+}
